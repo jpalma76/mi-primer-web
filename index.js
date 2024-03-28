f3aabd5f284fb7d635b07861214a9a04858aa95e
jpalma76 <juan.palma.alvarez@gmail.com>
2024-03-28 15:29:39 -0300

    elimina el alert, crea h1, agrega texto e inserta en div container

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-alert('Hola Platzi')
+alert("mensaje de prueba")
+
+const titulo = document.createElement('h1')
+titulo.innerHTML = "Hola mundo!"
+const container = document.querySelector('.container')
+container.appendChild(titulo)
